88c6a2f6a97e84088eaf07aaa99f6a1edc0b901c
Ramon Gabriel <ramongtstudy@gmail.com>
2023-11-24 22:38:43 -0300

    exercicio 4

--- projeto_final_garrido/src/app/app.module.ts
+++ projeto_final_garrido/src/app/app.module.ts
@@ -6,18 +6,26 @@ import { AppComponent } from './app.component';
 import { ContadorComponentComponent } from './contador-component/contador-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-
+import { FormCadastroComponent } from './form-cadastro/form-cadastro.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponentComponent
+    ContadorComponentComponent,
+    FormCadastroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    ToastrModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projeto_final_garrido/src/app/form-cadastro/form-cadastro.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class FormCadastroComponent implements OnInit {
+
+  public formLogin:FormGroup;
+
+  constructor(private fb:FormBuilder, private route:Router, private toast:ToastrService){
+    this.formLogin = this.criarFormLogin();
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public criarFormLogin():FormGroup{
+    return this.fb.group({
+      username:["", [Validators.required, Validators.minLength(6)]],
+      password:["", [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  public isFormControlInvalid(controlName: string):boolean{
+    return !!(this.formLogin.get(controlName)?.invalid && this.formLogin.get(controlName)?.touched)
+  }
+
+}
