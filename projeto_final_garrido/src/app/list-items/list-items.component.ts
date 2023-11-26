5cbe6ae353436eae843336779bcc6b84e5ed61c9
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2023-11-26 11:53:59 -0300

    Made 3° question

--- projeto_final_garrido/src/app/list-items/list-items.component.ts
+++ projeto_final_garrido/src/app/list-items/list-items.component.ts
@@ -1,13 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-list-items',
   templateUrl: './list-items.component.html',
   styleUrls: ['./list-items.component.css']
 })
 export class ListItemsComponent {
+  marked:number[]=[]
+  valor:string=''
 
   car: string[] = ["Ferrari 246 Dino", "Pontiac GTO", "Porsche 968", "Land Rover Defender", "Mazda RX-7 GS"];
-  
+  delete(index:number){
+    this.car.splice(index,1)
+    let presence = this.marked.indexOf(index)
+    if(presence!=-1){
+      this.marked.splice(presence,1)
+    }
+  }
+  done(index:number){
+    let presence = this.marked.indexOf(index)
+    if(presence===-1){
+      this.marked.push(index)
+    }
+  }
+  add(){
+    this.car.push(this.valor)
+  }
 }
